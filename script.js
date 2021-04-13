853739b961d4436beb92b1deee9da101130f0e0c
awe <a@awews.com>
2021-04-12 17:30:44 -0700

    draw 9  by 9 rectangle on the canvas to represent game board

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ console.log("loaded");
 const canvas = document.getElementById('quoridorBoard')
 if (canvas) {
     let ctx = canvas.getContext('2d')
-    
+    for (let i = 0; i < 9; i++) {
+        for (let j = 0; j < 9; j++) {
+            ctx.fillStyle = 'rgb('+ Math.floor(255 - 42.5 * i) + ', ' + Math.floor(255 - 42.5 * j) + ',0)';
+            ctx.fillRect(j * 25, i * 25, 25, 25);      
+        }        
+    }
 }
-console.log(`ctx`, ctx)
