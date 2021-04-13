0c25b7530e28077c31064b096aa536650dad22b1
awe <a@awews.com>
2021-04-12 17:54:19 -0700

    refactor to use helper func to draw all rectangles

--- script.js
+++ script.js
@@ -6,16 +6,22 @@ const boardHeight = canvas.height
 const squareColor = 'rgb(0,200,0, 0.95)'
 const boardDimension = 9
 
+// helper functions to create board structure
+function drawRect(ctx, startX, startY, rectWidth, rectHeight) {
+    ctx.fillRect(startX, startY, rectWidth, rectHeight)
+}
+
 if (canvas) {
     let ctx = canvas.getContext('2d')
     drawBoard(ctx, boardDimension, squareColor, boardWidth, boardHeight)
+    
 }
 
 function drawBoard(ctx, boardDimension, squareColor, boardWidth, boardHeight) {
     for (let i = 0; i < boardDimension; i++) {
         for (let j = 0; j < boardDimension; j++) {
             ctx.fillStyle = squareColor
-            ctx.fillRect(j * (boardWidth/boardDimension), i * (boardHeight/boardDimension), 25, 25);      
+            drawRect(ctx, j * (boardWidth/boardDimension), i * (boardHeight/boardDimension), 25, 25);      
         }        
     }
 }
