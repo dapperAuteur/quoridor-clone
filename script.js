65879e0c83df0df2a27674e92d4206565f0b2163
awe <a@awews.com>
2021-04-12 17:34:19 -0700

    define board dimensions using constants

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 console.log("loaded");
 const canvas = document.getElementById('quoridorBoard')
+// define board on canvas
+const boardWidth = canvas.width
+const boardHeight = canvas.height
+const boardDimension = 9
+
 if (canvas) {
     let ctx = canvas.getContext('2d')
-    for (let i = 0; i < 9; i++) {
-        for (let j = 0; j < 9; j++) {
+    for (let i = 0; i < boardDimension; i++) {
+        for (let j = 0; j < boardDimension; j++) {
             ctx.fillStyle = 'rgb('+ Math.floor(255 - 42.5 * i) + ', ' + Math.floor(255 - 42.5 * j) + ',0)';
             ctx.fillRect(j * 25, i * 25, 25, 25);      
         }        
