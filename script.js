f2e40c0bcf6154a3450a5b27032a930edc40dc44
awe <a@awews.com>
2021-04-12 17:35:54 -0700

    add board square color for player to place pawn

--- script.js
+++ script.js
@@ -3,13 +3,14 @@ const canvas = document.getElementById('quoridorBoard')
 // define board on canvas
 const boardWidth = canvas.width
 const boardHeight = canvas.height
+const squareColor = 'rgb(0,200,0, 0.95)'
 const boardDimension = 9
 
 if (canvas) {
     let ctx = canvas.getContext('2d')
     for (let i = 0; i < boardDimension; i++) {
         for (let j = 0; j < boardDimension; j++) {
-            ctx.fillStyle = 'rgb('+ Math.floor(255 - 42.5 * i) + ', ' + Math.floor(255 - 42.5 * j) + ',0)';
+            ctx.fillStyle = squareColor
             ctx.fillRect(j * 25, i * 25, 25, 25);      
         }        
     }
