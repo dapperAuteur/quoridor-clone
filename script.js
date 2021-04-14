5207f084a6673bb883e5dbd9b0f6809d62b1669a
awe <a@awews.com>
2021-04-14 14:22:36 -0700

    refactor to use const with alley and fence

--- script.js
+++ script.js
@@ -7,6 +7,14 @@ const squareColor = 'rgb(0,200,0, 0.95)'
 const squareWidth = 25
 const boardDimension = 9
 
+// define alleys on board
+const numAlleys = 10
+const alleyWidth = 16.11
+
+// define fence
+const fenceWidth = 16.11
+const fenceLength = 79.16 // 46.11 + 25 + 8.05
+
 // helper functions to create board structure
 function drawRect(ctx, startX, startY, rectWidth, rectHeight) {
     ctx.fillRect(startX, startY, rectWidth, rectHeight)
@@ -21,7 +29,7 @@ if (canvas) {
     for (let i = 0; i < boardDimension; i++) {
         for (let j = 0; j < boardDimension; j++) {
     ctx.fillStyle = "black"
-    drawRect(ctx, 30, 0, 16.11, 46.11 + 25 + 8.05);
+    drawRect(ctx, 30, 0, fenceWidth, fenceLength);
             ctx.fillStyle = "blue"
             drawRect(ctx, (j * 30) + 5, (i * 30) + 5, 25, 25);
         }        
