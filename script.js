126d467db6622894b4350af6a6bb295a9bb1db89
awe <a@awews.com>
2021-04-14 14:57:45 -0700

    draw rect for color between squares before drawing squares
    remove unused code

--- script.js
+++ script.js
@@ -35,30 +35,21 @@ function placeFenceHorizontally(ctx,fenceStartX, fenceStartY) {
 
 if (canvas) {
     let ctx = canvas.getContext('2d')
-    drawBoard(ctx, boardDimension, squareColor, boardWidth, boardHeight)
+    // draw game surface
+    drawBoard(ctx, boardDimension, boardWidth, boardHeight, squareColor)
+    // practice functions, not used in game
     drawRect(ctx, 0, 0, 10, 10, fenceColor)
     drawRect(ctx, 365, 0, 5, 10, fenceColor)
     // practice functions for drawing fences, not used in game
     placeFenceVertically(ctx, 30, 0)
     placeFenceHorizontally(ctx, 79.16, 30)
-        for (let j = 0; j < boardDimension; j++) {
-    ctx.fillStyle = "black"
-    drawRect(ctx, 30, 0, fenceWidth, fenceLength);
-            ctx.fillStyle = "blue"
-            drawRect(ctx, (j * 30) + 5, (i * 30) + 5, 25, 25);
-    drawRect(ctx, 30, 0, fenceWidth, fenceLength, "black");
-    // drawRect(ctx, 30, 0, fenceWidth, fenceLength);
-    for (let i = 0; i < numAlleys; i++) {
-        for (let j = 0; j < numAlleys; j++) {
-            drawRect(ctx, (j * 30), (i * 30), 10, 10, "blue");
-        }        
-    }
 }
 
-// move board over 10px or the width of one alley
-// Board x-coord should be at 5, 35, 65, 95, 125, 155, 185, 215, 245
-// Board y-coord should be at 5, 35, 65, 95, 125, 155, 185, 215, 245
+// draw board is the function used to draw the game board.
 function drawBoard(ctx, boardDimension, boardWidth, boardHeight, boardColor) {
+    // draw space between squares
+    drawRect(ctx, 0, 0, 370, 370, areaBetweenSquares)
+    // draw squares
     for (let i = 0; i < boardDimension; i++) {
         for (let j = 0; j < boardDimension; j++) {
             drawRect(ctx, (j * (boardWidth/boardDimension)) + 5, (i * (boardHeight/boardDimension)) + 5, squareWidth, squareWidth, boardColor);
